a65e1d538e5fb7f584ab10e3ad33e93d28250498
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 15:17:51 +0530

    Added:Equality check of two lines

--- src/com/programming/LineComparison.java
+++ src/com/programming/LineComparison.java
@@ -12,8 +12,19 @@ public class LineComparison {
         int y1= sc.nextInt();
         int x2= sc.nextInt();
         int y2= sc.nextInt();
+        System.out.println("Enter the co-ordinates for the line-2");
+        int X1= sc.nextInt();
+        int Y1= sc.nextInt();
+        int X2= sc.nextInt();
+        int Y2= sc.nextInt();
 
-        double lengthOfLine =Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2)));
+
+        double lengthOfLine1 =Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2)));
+        double lengthOfLine2 =Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2)));
+
+        if (lengthOfLine1==lengthOfLine2){
+            System.out.println("Both the lines are equal in length");
+        }
     }
 }
 
