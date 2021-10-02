85e7ad7852b3185e68505863c4e9e6617bda032a
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 15:44:58 +0530

    Added:Equality of two lines

--- src/com/programming/LineComparison.java
+++ src/com/programming/LineComparison.java
@@ -3,28 +3,44 @@ package com.programming;
 import java.util.Scanner;
 
 public class LineComparison {
-
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Welcome to Line length comparison computation program");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the co-ordinates for the line-1");
-        int x1= sc.nextInt();
-        int y1= sc.nextInt();
-        int x2= sc.nextInt();
-        int y2= sc.nextInt();
-        System.out.println("Enter the co-ordinates for the line-2");
-        int X1= sc.nextInt();
-        int Y1= sc.nextInt();
-        int X2= sc.nextInt();
-        int Y2= sc.nextInt();
+        LineComparison lineComparison = new LineComparison();
+        compareOfTwoLines();
 
-
-        double lengthOfLine1 =Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2)));
-        double lengthOfLine2 =Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2)));
-
-        if (lengthOfLine1==lengthOfLine2){
-            System.out.println("Both the lines are equal in length");
         }
+    public static double lengthOfLine(){
+        System.out.println("Enter the co-ordinate x1");
+        int x1= sc.nextInt();
+        System.out.println("Enter the co-ordinate y1");
+        int y1= sc.nextInt();
+        System.out.println("Enter the co-ordinate x2");
+        int x2= sc.nextInt();
+        System.out.println("Enter the co-ordinate y2");
+        int y2= sc.nextInt();
+
+        double lengthOfLine =Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2)));
+        return lengthOfLine;
+    }
+    public static void compareOfTwoLines(){
+
+        System.out.println("Enter the co-ordinates for the line-2");
+        double length1 = lengthOfLine();
+        System.out.println("Enter the co-ordinates for the line-2");
+        double length2 = lengthOfLine();
+
+        String s1 = String.valueOf(length1);
+        String s2 = String.valueOf(length2);
+
+        if (s1.equals(s2)){
+            System.out.println("Both the lines are equal");
+        }
+        else {
+            System.out.println("Lines are un-equal");
+        }
+    }
+}
     }
 }
 
