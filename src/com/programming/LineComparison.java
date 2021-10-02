ce6743e44a1d41a5fa6d575380952b6a6d6704c4
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 15:00:03 +0530

    Initial Commit:Welcome

---
+++ src/com/programming/LineComparison.java
@@ -0,0 +1,10 @@
+package com.programming;
+
+import java.util.Scanner;
+
+public class LineComparison {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line length comparison computation program");
+    }
+}
