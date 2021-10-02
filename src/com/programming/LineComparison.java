792145c702cef9d490a7ad5787c091e574c5a740
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 15:39:32 +0530

    Added: compute Length of line

--- src/com/programming/LineComparison.java
+++ src/com/programming/LineComparison.java
@@ -3,17 +3,25 @@ package com.programming;
 import java.util.Scanner;
 
 public class LineComparison {
+    Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Welcome to Line length comparison computation program");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the co-ordinates for the line-1");
-        int x1= sc.nextInt();
-        int y1= sc.nextInt();
-        int x2= sc.nextInt();
-        int y2= sc.nextInt();
+        lengthOfLine();
+    }
 
-        double lengthOfLine =Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2)));
+    public static double lengthOfLine() {
+        System.out.println("Enter the co-ordinate x1");
+        int x1 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y1");
+        int y1 = sc.nextInt();
+        System.out.println("Enter the co-ordinate x2");
+        int x2 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y2");
+        int y2 = sc.nextInt();
+
+        double lengthOfLine = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
+        return lengthOfLine;
     }
 }
 
